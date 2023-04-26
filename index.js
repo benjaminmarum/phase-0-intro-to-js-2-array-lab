9d1df407de777a82af798adfa3511ff02060e552
Ben Marum <ben.j.marum@gmail.com>
2023-04-25 19:46:40 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function  destructivelyAppendCat(name) {
+          cats.push(name);
+          return cats;
+};
+
+function  destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return cats;
+};
+
+function  destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+    return cats;
+};
+
+function  destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+    return cats;
+};
+
+
+function  appendCat(name) {
+    const kitties = [...cats];
+    kitties.push(name);
+    return kitties
+};
+
+function  prependCat(name) {
+    const kitties = [...cats];
+    kitties.unshift(name);
+    return kitties
+};
+
+function  removeLastCat(name) {
+    const kitties = [...cats];
+    kitties.pop(name);
+    return kitties
+};
+
+function  removeFirstCat(name) {
+    const kitties = [...cats];
+    kitties.shift(name);
+    return kitties
+};
+
+/*function restoreCatsArray(){
+    cats = ["Milo", "Otis", "Garfield"];
+}
+restoreCatsArray();
+*/
+
+
+
